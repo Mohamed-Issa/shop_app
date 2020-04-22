7e1e24f04c09bb796f088085be4bc198d6df49b8
mohamed <mohamed.hamdy.issa@gmail.com>
2020-04-22 06:22:54 +0200

    providing an orders object

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import './providers/orders.dart';
 import './screens/cart_screen.dart';
 import './providers/products_provider.dart';
 import './providers/cart.dart';
@@ -14,11 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider.value(
-          value: ProductsProvider(),
+        ChangeNotifierProvider(
+          create: (ctx) => ProductsProvider(),
         ),
         ChangeNotifierProvider.value(
           value: Cart(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Orders(),
         ),
       ],
       child: MaterialApp(
