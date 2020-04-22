9c3fd5a1c4f8734e8397b3a3a991b811f7e67bb8
mohamed <mohamed.hamdy.issa@gmail.com>
2020-04-22 14:58:00 +0200

    Snackbars & undoing 'Adding to cart' Actions

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -32,19 +32,39 @@ class Cart with ChangeNotifier {
     if (_items.containsKey(productId)) {
       _items.update(
           productId,
-          (existingCardItem) => CartItem(
-              id: existingCardItem.id,
-              title: existingCardItem.title,
-              price: existingCardItem.price,
-              quantity: existingCardItem.quantity + 1));
+              (existingCardItem) =>
+              CartItem(
+                  id: existingCardItem.id,
+                  title: existingCardItem.title,
+                  price: existingCardItem.price,
+                  quantity: existingCardItem.quantity + 1));
     } else {
       _items.putIfAbsent(
           productId,
-          () => CartItem(
-              id: DateTime.now().toString(),
-              price: price,
-              title: title,
-              quantity: 1));
+              () =>
+              CartItem(
+                  id: DateTime.now().toString(),
+                  price: price,
+                  title: title,
+                  quantity: 1));
+    }
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) return;
+    if (_items[productId].quantity > 1) {
+      _items.update(
+          productId,
+              (existingCartItem) =>
+              CartItem(
+                  id: existingCartItem.id,
+                  title: existingCartItem.title,
+                  quantity: existingCartItem.quantity - 1,
+                  price: existingCartItem.price));
+    }
+    else {
+      _items.remove(productId);
     }
     notifyListeners();
   }
