d1ce78d5ece9d6f2f0f966d0aae4d37ffadf99cc
mohamed <mohamed.hamdy.issa@gmail.com>
2020-04-25 20:36:12 +0200

    allowing users to log in

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,9 +9,9 @@ class Auth with ChangeNotifier {
   String _expiryDate;
   String _userId;
 
-  Future<void> signUp(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoEhEKTrzxPEUmHB4CgMbXdca5yOMKuZM';
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDoEhEKTrzxPEUmHB4CgMbXdca5yOMKuZM';
     final response = await http.post(url,
         body: json.encode({
           'email': email,
@@ -18,5 +19,31 @@ class Auth with ChangeNotifier {
           'returnSecureToken': true,
         }));
     print(json.decode(response.body));
+  }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+//    const url =
+//        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDoEhEKTrzxPEUmHB4CgMbXdca5yOMKuZM';
+//    final response = await http.post(url,
+//        body: json.encode({
+//          'email': email,
+//          'password': password,
+//          'returnSecureToken': true,
+//        }));
+//    print(json.decode(response.body));
+  }
+
+  Future<void> logIn(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+//    const url =
+//        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDoEhEKTrzxPEUmHB4CgMbXdca5yOMKuZM';
+//    final response = await http.post(url,
+//        body: json.encode({
+//          'email': email,
+//          'password': password,
+//          'returnSecureToken': true,
+//        }));
+//    print(json.decode(response.body));
   }
 }
